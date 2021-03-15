160cbd0483ac65f90250a93a5f430a362ebb309d
hanl choi <hn.choi@cashwalk.io>
2021-03-15 18:26:26 +0900

    init

--- app/src/main/java/com/h2ve/smallhabits/MainActivity.kt
+++ app/src/main/java/com/h2ve/smallhabits/MainActivity.kt
@@ -6,6 +6,25 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val uid = binding.id.text.toString()
+            val upw = binding.pw.text.toString()
+
+
+        }
+
+        binding.signupBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.skipBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, BoardActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
